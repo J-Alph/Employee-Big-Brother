67d564563828c3b8bfd43bc3c42fed305949dc0a
Alphonse Johnson <a.joh26@gmail.com>
2023-05-14 17:26:51 -0500

    Built the tables to hold the data

--- db/schema.sql
+++ db/schema.sql
@@ -9,4 +9,23 @@ CREATE TABLE department (
   )
 
 
-  CREATE TABLE role 
+  CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+  ) 
+
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+)
