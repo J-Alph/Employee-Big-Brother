e9d3a6913c000e2acecdefde4ae25f80d56e1e6b
Alphonse Johnson <a.joh26@gmail.com>
2023-05-20 23:11:00 -0500

    added more queries to manage the db

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 // Import and require mysql2
 const mysql = require("mysql2");
+const pass = require('dotenv').config();
 
 const inquirer = require("inquirer");
 
 // import {mypass} from 'env';
-
-const viewEmployee = require("./utils/answerchoices");
 
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -20,7 +19,7 @@ const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    password: "Flush@13",
+    password: process.env.DB_PASSWORD,
     database: "staff_db",
   },
   console.log(`Connected to the books_db database.`)
@@ -34,7 +33,7 @@ const questions = [
     choices: [
       "View all employees",
       "add employee",
-      "update Employee",
+      "update employee role",
       "view all roles",
       "add role",
       "view all departments",
@@ -47,6 +46,18 @@ const questions = [
 inquirer.prompt(questions).then((answers) => {
   if (answers.options === "View all employees") {
     db.query("SELECT * FROM employee", function (err, results) {
+      return console.table(results);
+    });
+  }
+
+  if (answers.options === "view all departments") {
+    db.query("SELECT * FROM department", function (err, results) {
+      return console.table(results);
+    });
+  }
+
+  if (answers.options === "view all roles") {
+    db.query("SELECT * FROM roles", function (err, results) {
       return console.table(results);
     });
   }
@@ -66,24 +77,95 @@ inquirer.prompt(questions).then((answers) => {
 
   if (answers.options === "add employee") {
     inquirer.prompt(questions2).then((answers) => {
-      //   const questions2 = [  {
-      //   type: "input",
-      //   name: "title",
-      //   message: "What is the name of the employee?",
-      // }]
-
       db.query(
         "INSERT INTO employee SET ?",
         { first_name: answers.name, last_name: answers.lastname },
         (err, res) => {
           if (err) throw err;
           console.log("Employee has been added");
-          runPrompt();
         }
       );
     });
   }
+
   const questions3 = [
+    {
+      type: "input",
+      name: "rolename",
+      message: "What is the title of the new role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the salary of the new role?",
+    },
+  ];
+
+  if (answers.options === "add role") {
+    inquirer.prompt(questions3).then((answers) => {
+      db.query(
+        "INSERT INTO roles SET ?",
+        { title: answers.rolename, salary: answers.salary },
+        (err, res) => {
+          if (err) throw err;
+          console.log("role has been added");
+        }
+      );
+    });
+  }
+
+  const questions4 = [
+    {
+      type: "input",
+      name: "departmentname",
+      message: "What is the name of the new department",
+    },
+  ];
+
+  if (answers.options === "add department") {
+    inquirer.prompt(questions4).then((answers) => {
+      db.query(
+        "INSERT INTO department SET ?",
+        { name: answers.departmentname },
+        (err, res) => {
+          if (err) throw err;
+          console.log("Department has been added");
+        }
+      );
+    });
+  }
+
+  const questions5 = [
+    {
+      type: "input",
+      name: "roleupdate",
+      message: "What is the new role to update?",
+    },
+
+    {
+      type: "input",
+      name: "roleid",
+      message: "Which role id would like to update?",
+    },
+  ];
+
+  if (answers.options === "update employee role") {
+    inquirer.prompt(questions5).then((answers) => {
+      db.query(
+        `UPDATE roles SET title = ? WHERE id = ?`,
+        [answers.roleupdate, answers.roleid],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log(result);
+          inquirer.prompt();
+        }
+      );
+    });
+  }
+
+  const questions6 = [
     {
       type: "input",
       name: "delete",
@@ -92,7 +174,7 @@ inquirer.prompt(questions).then((answers) => {
   ];
 
   if (answers.options === "delete an employee") {
-    inquirer.prompt(questions3).then((answers) => {
+    inquirer.prompt(questions6).then((answers) => {
       db.query(
         `DELETE FROM employee WHERE id = ?`,
         answers.delete,
@@ -101,14 +183,9 @@ inquirer.prompt(questions).then((answers) => {
             console.log(err);
           }
           console.log(result);
+          inquirer.prompt();
         }
       );
-    });
-  }
-
-  if (answers.options === "view all departments") {
-    db.query("SELECT * FROM department", function (err, results) {
-      return console.log(results);
     });
   }
 }); //the whole block
--- utils/answerchoices.js
+++ utils/answerchoices.js
@@ -1,15 +1,91 @@
-function viewEmployee(options){
+// const mysql = require("mysql2");
 
-    if(options){
-        const query ={
-            "add employee": 'nothing new'
+// const inquirer = require("inquirer");
 
-        }
+
+  
+
+// function Employee(data){
+
+//     if (data.options === "add employee") {
+//         inquirer.prompt(questions2)
+//           db.query(
+//             "INSERT INTO employee SET ?",
+//             { first_name: data.name, last_name: data.lastname },
+//             (err, res) => {
+//               if (err) throw err;
+//               console.log("Employee has been added");
+            
+//             }
+//           );
+//        ;
+//       }
+
+
+
+
+
+
+// }
+
+    // if (data.options === "add employee") {
+    //     inquirer.prompt(questions2)
+    //       db.query(
+    //         "INSERT INTO employee SET ?",
+    //         { first_name: data.name, last_name: data.lastname },
+    //         (err, res) => {
+    //           if (err) throw err;
+    //           console.log("Employee has been added");
+            
+    //         }
+    //       );
+    //    ;
+    //   }
+
+    //   const questions4 = [
+    //     {
+    //       type: "input",
+    //       name: "update",
+    //       message: "Which employee would you like to update?",
+    //     },
+    //   ];
+
+
+
+
+    //   if (data.options === "update Employee") {
+    //     inquirer.prompt(questions4)
+    //       db.query(
+    //         "UPDATE employee SET first_name, last_name",
+    //         { first_name: data.name, last_name: data.lastname },
+    //         (err, res) => {
+    //           if (err) throw err;
+    //           console.log("Employee changed");
+            
+    //         }
+    //       );
+    //    ;
+    //   }
+
+
+    //   if (data.options === "add employee") {
+    //     inquirer.prompt(questions2)
+    //       db.query(
+    //         "INSERT INTO employee SET ?",
+    //         { first_name: data.name, last_name: data.lastname },
+    //         (err, res) => {
+    //           if (err) throw err;
+    //           console.log("Employee has been added");
+            
+    //         }
+    //       );
+    //    ;
+    //   }
+
+
+
+
+
+
     
-        return `SELECT * FROM employee`;
-        
-}
-} 
-
-
-module.exports = viewEmployee;
+// module.exports = Employee;
